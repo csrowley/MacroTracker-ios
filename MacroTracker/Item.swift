bc984f410de521236e038d9950e268214fea5fcd
Chris Rowley <csrowley@uci.edu>
2024-06-20 00:40:43 -0700

    Initial Commit

---
+++ MacroTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MacroTracker
+//
+//  Created by Chris Rowley on 6/20/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
